c285b31f2f073ff6779db59eaf3cd55c37eb1b18
Hiba Abo Shawish <Hiba.Aboshawish@triosstudent.com>
2026-06-07 10:14:18 -0400

    Initial Commit

---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Hiba Abo Shawish on 2026-06-07.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScavengerHuntTests/ScavengerHuntTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntTests.swift
+//  ScavengerHuntTests
+//
+//  Created by Hiba Abo Shawish on 2026-06-07.
+//
+
+import Testing
+@testable import ScavengerHunt
+
+struct ScavengerHuntTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
